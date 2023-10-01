cf7d4eb54f099b742007faaaec8f78e6d17d6948
Stefan Pawlowski <Stefan.Pawlowski@Infinithings.de>
2023-10-01 15:07:23 +0200

    feat: Initial Structure

---
+++ README.txt
@@ -0,0 +1,5 @@
+Snake The Game
+===============
+Once upon a time, there was a hungry Snake...
+
+This is just a projec to experiment with the Godot 4 Engine.
---
+++ Scenes/MainMenu/scripts/MenuConfig.gd
@@ -0,0 +1,23 @@
+extends MenuButton
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	self.get_popup().allow_search = false
+
+	self.get_popup().add_item("Einfach", 1)
+	self.get_popup().add_item("Mittel", 2)
+	self.get_popup().add_item("Schwer", 3)
+	
+	self.get_popup().connect("id_pressed", self._startGame)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+func _startGame(id):
+	
+	print("Hi "+ str(id))
